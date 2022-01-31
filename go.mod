75084ab65241f13f166c30811e4abc572f62cf40
Zhenyuan Wang <zhenyuan_wang@foxmail.com>
2022-01-31 09:13:40 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xwzy/demo-govaluate
+
+go 1.17
+
+require github.com/Knetic/govaluate v3.0.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Knetic/govaluate"
+)
+
+
+func main() {
+	Test01()
+	Test02()
+	Test03()
+}
+
+func Test01() {
+	expression, err := govaluate.NewEvaluableExpression("10 > 0")
+	result, err := expression.Evaluate(nil)
+	fmt.Println(result)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Test02() {
+	expression, err := govaluate.NewEvaluableExpression("foo > 0")
+
+	parameters := make(map[string]interface{}, 8)
+	parameters["foo"] = -1
+
+	result, err := expression.Evaluate(parameters)
+	fmt.Println(result)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Test03() {
+	expression, err := govaluate.NewEvaluableExpression("(requests_made * requests_succeeded / 100) >= 90")
+
+	parameters := make(map[string]interface{}, 8)
+	parameters["requests_made"] = 100
+	parameters["requests_succeeded"] = 80
+
+	result, err := expression.Evaluate(parameters)
+
+	fmt.Println(result)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Test04() {
+	expression, err := govaluate.NewEvaluableExpression("http_response_body == 'service is ok'")
+
+	parameters := make(map[string]interface{}, 8)
+	parameters["http_response_body"] = "service is ok"
+
+	result, err := expression.Evaluate(parameters)
+
+}
